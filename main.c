af88853b24b0183c0538074bc2cd6453297dd1be
Jasper Tan <jaspertan@OS-Fa23.myguest.virtualbox.org>
2023-09-09 20:57:14 -0500

    Added parse_input function

--- main.c
+++ main.c
@@ -14,6 +14,8 @@
 #define MAX_TOKENS 1000
 #define MAX_ARG_CHARS 30
 
+void parse_input(char *input_address, int file_descriptors[][3], char *args[][MAX_TOKENS], int *output);
+
 void execute_command(int file_descriptors[][3], char *args[]);
 
 void pipe_command(int file_descriptors[][3], char *args[][(MAX_TOKENS)]);
@@ -27,7 +29,6 @@ int main()
 
     char input[MAX_INPUTS];
     char *token = (char *)malloc((MAX_ARG_CHARS) * sizeof(char));
-    char *command_args = (char *)malloc((MAX_INPUTS) * sizeof(char));
     char *args[2][MAX_TOKENS];
 
     int row;
@@ -35,7 +36,7 @@ int main()
     int store_token_flag;  // flag to stop storing tokens
     int exit_flag;         // 0: dont exit, 1: exit
     int next_command_flag; // 0: countinue, 1: next token
-    int no_token_flag;     // 1: no token, 0: token exist
+    int output_flags[2];
 
     int redirect_input_flag = 0;  // idx of "<"
     int redirect_output_flag = 0; // idx of ">"
@@ -44,7 +45,10 @@ int main()
 
     int prev_descriptor; // previous descriptor
 
-    do
+    struct Job
+    {
+
+    } do
     {
         // Get user input
         printf("#");
@@ -62,111 +66,14 @@ int main()
         // Parse inputs for tokens with delimeters of ' '
         char *input_address = input;
 
-        // Dictate which row to save in arg matrix
-        row = 0;
+        parse_input(input_address, file_descriptors, args, output_flags);
 
-        // Reset Token count
-        token_num = 0;
+        row = output_flags[0];
 
-        // Set flag when token reaches a symbol
-        store_token_flag = 1;
-
-        // Set flag when file descriptor is incorrect
-        next_command_flag = 0;
-
-        no_token_flag = 1;
-
-        // Parse Tokens in input
-        while (token = strtok_r(input_address, " ", &input_address))
-        {
-            no_token_flag = 0;
-
-            // If there is more than one pipe
-            if (row == 2)
-            {
-                break;
-            }
-            else if (strcmp(token, "|") == 0)
-            {
-                // Set flag when token reaches a symbol
-                store_token_flag = 1;
-
-                // Set the last token to NULL for execvp to run
-                args[row][token_num] = NULL;
-
-                // Increment row for next pipe
-                row++;
-
-                // Reset Token count
-                token_num = 0;
-            }
-            else
-            {
-                // Set off flag whenever one of the redirect tokens are parsed
-                store_token_flag = (strcmp(token, "<") == 0 || strcmp(token, ">") == 0 || strcmp(token, "2>") == 0) ? 0 : store_token_flag;
-
-                // Parse command
-                if (store_token_flag)
-                {
-                    // Allocate memory for each token parsed
-                    args[row][token_num] = (char *)malloc((strlen(token) * sizeof(char)));
-                    args[row][token_num] = token;
-
-                    // Increment number of tokens
-                    token_num++;
-
-                    // Set the last token to NULL for execvp to run
-                    args[row][token_num] = NULL;
-                }
-                else
-                {
-                    // Depeneding on which redirect flag, open corresponding file descriptor
-                    if (redirect_input_flag)
-                    {
-                        file_descriptors[row][0] = open(token, O_RDONLY);
-                        if (file_descriptors[row][0] == -1)
-                        {
-                            perror("no directory");
-                            next_command_flag = 1;
-                        }
-
-                        redirect_input_flag = 0;
-                    }
-                    else if (redirect_output_flag)
-                    {
-                        file_descriptors[row][1] = open(token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-                        if (file_descriptors[row][1] == -1)
-                        {
-                            perror("no directory");
-                            next_command_flag = 1;
-                        }
-                        redirect_output_flag = 0;
-                    }
-                    else if (redirect_error_flag)
-                    {
-                        file_descriptors[row][2] = open(token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-                        if (file_descriptors[row][2] == -1)
-                        {
-                            perror("no directory");
-                            next_command_flag = 1;
-                        }
-
-                        redirect_error_flag = 0;
-                    }
-                    else
-                    {
-                        // Set Flags when symbols are in token
-                        redirect_input_flag = (strcmp(token, "<") == 0) ? 1 : redirect_input_flag;
-                        redirect_output_flag = (strcmp(token, ">") == 0) ? 1 : redirect_output_flag;
-                        redirect_error_flag = (strcmp(token, "2>") == 0) ? 1 : redirect_error_flag;
-                    }
-                }
-            }
-        }
-        row++;
+        next_command_flag = output_flags[1];
 
         // Check if user wants to exit
-        if (!no_token_flag)
+        if (!next_command_flag)
         {
             if (strcmp(args[0][0], "exit") == 0)
             {
@@ -177,16 +84,131 @@ int main()
                 pipe_command(file_descriptors, args);
                 close_fd(file_descriptors);
             }
-            else if (!next_command_flag) // Regular command
+            else // Regular command
             {
                 execute_command(file_descriptors, args[0]);
                 close_fd(file_descriptors);
             }
         }
-    } while (!exit_flag);
+    }
+    while (!exit_flag)
+        ;
 
     // Free allocated memory
     free(token);
+}
+
+void parse_input(char *input_address, int file_descriptors[][3], char *args[][MAX_TOKENS], int output_flags[2])
+{
+    int redirect_input_flag = 0;  // idx of "<"
+    int redirect_output_flag = 0; // idx of ">"
+    int redirect_error_flag = 0;  // idx of "2>"
+
+    // Dictate which row to save in arg matrix
+    int row = 0;
+
+    // Reset Token count
+    int token_num = 0;
+
+    // Set flag when token reaches a symbol
+    int store_token_flag = 1;
+
+    // Set flag when file descriptor is incorrect
+    int next_command_flag = 1;
+
+    char *token = (char *)malloc((MAX_ARG_CHARS) * sizeof(char));
+    // Parse Tokens in input
+    while (token = strtok_r(input_address, " ", &input_address))
+    {
+        next_command_flag = 0;
+
+        // If there is more than one pipe
+        if (row == 2)
+        {
+            break;
+        }
+        else if (strcmp(token, "|") == 0)
+        {
+            // Set flag when token reaches a symbol
+            store_token_flag = 1;
+
+            // Set the last token to NULL for execvp to run
+            args[row][token_num] = NULL;
+
+            // Increment row for next pipe
+            row++;
+
+            // Reset Token count
+            token_num = 0;
+        }
+        else
+        {
+            // Set off flag whenever one of the redirect tokens are parsed
+            store_token_flag = (strcmp(token, "<") == 0 || strcmp(token, ">") == 0 || strcmp(token, "2>") == 0) ? 0 : store_token_flag;
+
+            // Parse command
+            if (store_token_flag)
+            {
+                // Allocate memory for each token parsed
+                args[row][token_num] = (char *)malloc((strlen(token) * sizeof(char)));
+                args[row][token_num] = token;
+
+                // Increment number of tokens
+                token_num++;
+
+                // Set the last token to NULL for execvp to run
+                args[row][token_num] = NULL;
+            }
+            else
+            {
+                // Depeneding on which redirect flag, open corresponding file descriptor
+                if (redirect_input_flag)
+                {
+                    file_descriptors[row][0] = open(token, O_RDONLY);
+                    if (file_descriptors[row][0] == -1)
+                    {
+                        perror("no directory");
+                        next_command_flag = 1;
+                    }
+
+                    redirect_input_flag = 0;
+                }
+                else if (redirect_output_flag)
+                {
+                    file_descriptors[row][1] = open(token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+                    if (file_descriptors[row][1] == -1)
+                    {
+                        perror("no directory");
+                        next_command_flag = 1;
+                    }
+                    redirect_output_flag = 0;
+                }
+                else if (redirect_error_flag)
+                {
+                    file_descriptors[row][2] = open(token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+                    if (file_descriptors[row][2] == -1)
+                    {
+                        perror("no directory");
+                        next_command_flag = 1;
+                    }
+
+                    redirect_error_flag = 0;
+                }
+                else
+                {
+                    // Set Flags when symbols are in token
+                    redirect_input_flag = (strcmp(token, "<") == 0) ? 1 : redirect_input_flag;
+                    redirect_output_flag = (strcmp(token, ">") == 0) ? 1 : redirect_output_flag;
+                    redirect_error_flag = (strcmp(token, "2>") == 0) ? 1 : redirect_error_flag;
+                }
+            }
+        }
+    }
+    row++;
+    free(token);
+
+    output_flags[0] = row;
+    output_flags[1] = next_command_flag;
 }
 
 void execute_command(int file_descriptors[][3], char *args[])
